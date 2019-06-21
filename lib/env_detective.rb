c4d420e3e73cb81d7586c3ba8f9671e5c039acf9
grezar <grezar.dev@gmail.com>
2019-06-21 21:30:58 +0900

    Output test log to tmp/env_detective.log

--- lib/env_detective.rb
+++ lib/env_detective.rb
@@ -1,4 +1,2 @@
 require "env_detective/version"
 require "env_detective/extension"
-
-ENV = EnvDetective::Extension.new(ENV)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,10 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "logger"
 require "env_detective"
 
 require "minitest/autorun"
+
+file = File.open(File.join(__dir__, '../tmp/env_detective.log'), File::WRONLY | File::CREAT)
+ext = EnvDetective::Extension.new(ENV)
+ext.logger = Logger.new(file, 0)
+ENV = ext
